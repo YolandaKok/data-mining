50d7875efa40a8963d21c853522000f926af132e
YolandaKok <yolanda.kokkinou@gmail.com>
2018-04-05 15:31:01 +0300

    10 fold cross validation

--- cross_validation.py
+++ cross_validation.py
@@ -16,14 +16,39 @@ import nltk
 from nltk import PorterStemmer
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectPercentile, f_classif
 
 # Main Program
+# Classifier
+clf =  RandomForestClassifier()
+
 train_set = pd.read_csv('train_set.csv', sep="\t", encoding = 'utf8')
 train_set = train_set[0:100]
+train_set_content = train_set['Content']
+train_set_categories = train_set['Category']
 
-kf = KFold(n_splits=2)
-kf.get_n_splits(train_set)
-for train_index, test_index in kf.split(X):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+kf = KFold(n_splits=10)
+for train_indexes, test_indexes in kf.split(train_set):
+    #print("TRAIN:", train_indexes, "TEST:", test_indexes)
+    features_train = [train_set_content[i] for i in train_indexes]
+    features_test = [train_set_content[i] for i in test_indexes]
+    categories_train = [train_set_categories[i] for i in train_indexes]
+    categories_test = [train_set_categories[i] for i in test_indexes]
+
+    # Pipeline for features_train -> Content of every article
+    vectorizer = TfidfVectorizer(stop_words='english')
+    features_train_transformed = vectorizer.fit_transform(features_train)
+    features_test_transformed = vectorizer.transform(features_test)
+    # Select only ten from it
+    selector = SelectPercentile(f_classif, percentile = 10)
+    selector.fit(features_train_transformed, categories_train)
+    features_train_transformed = selector.transform(features_train_transformed).toarray()
+    #print features_train_transformed
+    features_test_transformed = selector.transform(features_test_transformed).toarray()
+    #print features_test_transformed
+    clf.fit(features_train_transformed, categories_train)
+    prediction = clf.predict(features_test_transformed)
+    acc = accuracy_score(prediction, categories_test)
+
+    print acc
